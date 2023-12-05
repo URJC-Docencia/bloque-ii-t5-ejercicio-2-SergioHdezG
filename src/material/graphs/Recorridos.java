0168e004c4e78726ba54db712042805285cceaab
sergio.hernandez <sergio.hernandez@urjc.es>
2023-12-05 12:45:20 +0100

    Código día 05/12/2023

--- src/material/graphs/Recorridos.java
+++ src/material/graphs/Recorridos.java
@@ -1,8 +1,10 @@
 package material.graphs;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
+import material.Position;
 import material.tree.Tree;
+import material.tree.narytree.LinkedTree;
 
 /**
  *
@@ -21,7 +23,27 @@ public class Recorridos<V, E> {
      */
     
     public Tree<Vertex<V>> depthTravel(Graph<V,E> graph, Vertex<V> source){
-        throw new UnsupportedOperationException ("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        LinkedTree<Vertex<V>> tree = new LinkedTree<>();
+        Deque<Position<Vertex<V>>> stack = new ArrayDeque<>();
+        Set<Vertex<V>> visited = new HashSet<>();
+
+        Position<Vertex<V>> aux = tree.addRoot(source);
+        stack.addFirst(aux);
+        visited.add(source);
+        
+        while (!stack.isEmpty()){
+            Position<Vertex<V>> current = stack.pop();
+            Vertex<V> vertex = aux.getElement();
+            for (Edge<E> edge : graph.incidentEdges(vertex)){
+                Vertex<V> opp = graph.opposite(current.getElement(), edge);
+                if (!visited.contains(opp)) {
+                    Position<Vertex<V>> inserted = tree.add(opp, current);
+                    stack.addFirst(inserted);
+                    visited.add(opp);
+                }
+            }
+        }
+        return tree;
     }
     
     /**
@@ -33,7 +55,30 @@ public class Recorridos<V, E> {
      */
     
     public Tree<Vertex<V>> widthTravel(Graph<V,E> graph, Vertex<V> source){
-        throw new UnsupportedOperationException ("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        LinkedTree<Vertex<V>> tree = new LinkedTree<>();
+        Deque<Position<Vertex<V>>> queue = new ArrayDeque<>();
+        Set<Vertex<V>> visited = new HashSet<>();
+        
+        Position<Vertex<V>> root = tree.addRoot(source);
+        queue.add(root);
+        visited.add(source);
+
+        while (queue.size() > 0){
+            Position<Vertex<V>> current = queue.pop();
+            Collection <? extends Edge <E>> adjacents = graph.incidentEdges(current.getElement());
+            
+            for (Edge<E> edge: adjacents){
+                
+                Vertex<V> opp = graph.opposite(current.getElement(), edge);
+                
+                if (!visited.contains(opp)) {
+                    Position<Vertex<V>> inserted = tree.add(opp, current);
+                    queue.push(inserted);
+                    visited.add(opp);
+                }
+            }
+        }
+        return tree;
     }
     
     /**
@@ -45,7 +90,24 @@ public class Recorridos<V, E> {
      * @return The edge list
      */
     public List<Edge<E>> getPath(Graph<V, E> graph, Vertex<V> startVertex, Vertex<V> endVertex) {
-       throw new UnsupportedOperationException ("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       List<Edge<E>> edgeList = new ArrayList<>();
+       Tree<Vertex<V>> tree = widthTravel(graph, startVertex);
+
+       Iterator<Position<Vertex<V>>> it = tree.iterator();
+
+       Position<Vertex<V>> aux = null;
+
+       while (it.hasNext() && (aux == null || !aux.getElement().equals(endVertex))) {
+           aux = it.next();
+       }
+
+       while (!tree.isRoot(aux)){
+           Position<Vertex<V>> parent = tree.parent(aux);
+           Edge<E> edge = graph.areAdjacent(aux.getElement(), parent.getElement());
+           edgeList.add(0, edge);
+           aux = parent;
+       }
+       return edgeList;
     }
 
     /**
@@ -55,7 +117,13 @@ public class Recorridos<V, E> {
      * @return 
      */
     public Set<Vertex<V>> traverse(Graph<V, E> g, Vertex<V> root) {
-        throw new UnsupportedOperationException ("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Set<Vertex<V>> vertexSet = new HashSet<>();
+        Tree<Vertex<V>> tree = widthTravel(g, root);
+
+        for (Position<Vertex<V>> pos : tree){
+            vertexSet.add(pos.getElement());
+        }
+        return vertexSet;
     }
     
     
